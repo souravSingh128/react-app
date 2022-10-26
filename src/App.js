db38ed9f5f39abbb5366f6ddf1e29880dc023f25
souravSingh128 <81309000+souravSingh128@users.noreply.github.com>
2022-10-26 16:35:10 +0530

    for build start

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload ten times again &
-          again by praveen.
+          again by praveen start build.
         </p>
         <a
           className="App-link"
