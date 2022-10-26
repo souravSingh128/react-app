92865c0f3c9bbaf8d97541619e73b39fd629cf83
Sourav Singh <sourav.singh@winfosolutions.com>
2022-10-26 16:44:00 +0530

    something

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload ten times again &
-          again by praveen.
+          Edit <code>src/App.js</code> demo react app in azure web app again .
         </p>
         <a
           className="App-link"
